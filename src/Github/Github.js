9dae3b97e1a42ed16dbe20b3724a3f9e05c2de90
Jodde <jodde1980@aol.com>
2018-10-23 17:46:14 -0600

    Fixed some styling.

--- src/Github/Github.js
+++ src/Github/Github.js
@@ -6,7 +6,7 @@ class Github extends React.Component
     render()
     {
         return(
-            <Profile username={'samdoj'}/>
+            <Profile username={'samdoj'} className="push-up"/>
                 )
     }
 }
