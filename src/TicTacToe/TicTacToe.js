21b75b808bc423c8fab21b2e7b0f80ee1c0e02e9
Jodde <jodde1980@aol.com>
2018-10-24 21:22:34 -0600

    Put tic-tac-toe on Netlify

--- src/TicTacToe/TicTacToe.js
+++ src/TicTacToe/TicTacToe.js
@@ -4,7 +4,7 @@ export default class TicTacToe extends React.Component
 {
     render() {
         return (
-            <iframe title = "Tic Tac Toe" src={'https://tictactoe.joedmasonsd.com'} frameBorder="0" style={{position:'absolute',bottom:'0', width:'100%', height:'90vh'}}/>
+            <iframe title = "Tic Tac Toe" src={'https://romantic-tereshkova-e4803c.netlify.com/'} frameBorder="0" style={{position:'absolute',bottom:'0', width:'100%', height:'90vh'}}/>
         )
     }
 }
